e19d18fa20ec56866b5a75ed6cf2f3753af205ac
vladjkezor <savinv31@gmail.com>
2024-10-07 10:16:51 +0300

    fixing previous commit

--- 02/test_retry_deco.py
+++ 02/test_retry_deco.py
@@ -8,18 +8,48 @@ from retry_deco import retry_deco
 class TestRetryDecorator(unittest.TestCase):
 
     @patch('builtins.print')
-    def test_first_attempt_success(self, mock_print):
+    def test_first_attempt_success_with_args(self, mock_print):
         @retry_deco(3)
         def add(a, b):
             return a + b
 
         add(2, 3)
 
-        expected_print = mock.call('run "add", ',
-                                   'positional args = (2, 3), ',
-                                   '', 'attempt=1, result=5', sep='')
+        calls = mock.call('run "add", ',
+                          'positional args = (2, 3), ',
+                          '', 'attempt=1, result=5', sep='')
 
-        self.assertEqual(mock_print.call_args, expected_print)
+        self.assertEqual(mock_print.call_args, calls)
+        self.assertEqual(mock_print.call_count, 1)
+
+    @patch('builtins.print')
+    def test_first_attempt_success_with_kwargs(self, mock_print):
+        @retry_deco(3)
+        def add(a, b):
+            return a + b
+
+        add(a=10, b=23)
+
+        calls = mock.call('run "add", ', '',
+                          "keyword kwargs = {'a': 10, 'b': 23}, ",
+                          'attempt=1, result=33', sep='')
+
+        self.assertEqual(mock_print.call_args, calls)
+        self.assertEqual(mock_print.call_count, 1)
+
+    @patch('builtins.print')
+    def test_first_attempt_success_full_args(self, mock_print):
+        @retry_deco(3)
+        def add(a, b, c, d):
+            return a + b + c + d
+
+        add(100, 5, c=7, d=15)
+
+        calls = mock.call('run "add", ', 'positional args = (100, 5), ',
+                          "keyword kwargs = {'c': 7, 'd': 15}, ",
+                          'attempt=1, result=127', sep='')
+
+        self.assertEqual(mock_print.call_args, calls)
         self.assertEqual(mock_print.call_count, 1)
 
     @patch('builtins.print')
@@ -32,25 +62,25 @@ class TestRetryDecorator(unittest.TestCase):
             return isinstance(value, int)
 
         check_int(value=1)
-        expected_print = mock.call('run "check_int", ',
-                                   '', "keyword kwargs = {'value': 1}, ",
-                                   'attempt=1, result=True', sep='')
+        calls = mock.call('run "check_int", ',
+                          '', "keyword kwargs = {'value': 1}, ",
+                          'attempt=1, result=True', sep='')
 
-        self.assertEqual(mock_print.call_args, expected_print)
+        self.assertEqual(mock_print.call_args, calls)
         self.assertEqual(mock_print.call_count, 1)
 
         mock_print.reset_mock()
         check_int(value=None)
 
-        expected_print = mock.call('run "check_int", ', '',
-                                   "keyword kwargs = {'value': None}, ",
-                                   'attempt=1, exception = ValueError', sep='')
+        calls = mock.call('run "check_int", ', '',
+                          "keyword kwargs = {'value': None}, ",
+                          'attempt=1, exception = ValueError', sep='')
 
-        self.assertEqual(mock_print.call_args, expected_print)
+        self.assertEqual(mock_print.call_args, calls)
         self.assertEqual(mock_print.call_count, 1)
 
     @patch('builtins.print')
-    def test_count_of_retries(self, mock_print):
+    def test_multiple_retries(self, mock_print):
 
         @retry_deco(3)
         def check_str(value=None):
@@ -72,3 +102,33 @@ class TestRetryDecorator(unittest.TestCase):
 
         self.assertEqual(mock_print.call_args_list, calls)
         self.assertEqual(mock_print.call_count, 3)
+
+    @patch('builtins.print')
+    def test_zero_retries(self, mock_print):
+
+        @retry_deco(0)
+        def check_str(value=None):
+            if value is None:
+                raise ValueError()
+            return isinstance(value, str)
+
+        calls = []
+
+        check_str(value=None)
+
+        self.assertEqual(mock_print.call_args_list, calls)
+        self.assertEqual(mock_print.call_count, 0)
+
+    @patch('builtins.print')
+    def test_first_attempt_success_zero_args(self, mock_print):
+        @retry_deco(3)
+        def hello():
+            return "Hello Python"
+
+        hello()
+
+        calls = mock.call('run "hello", ', '',
+                          '', "attempt=1, result='Hello Python'", sep='')
+
+        self.assertEqual(mock_print.call_args, calls)
+        self.assertEqual(mock_print.call_count, 1)
