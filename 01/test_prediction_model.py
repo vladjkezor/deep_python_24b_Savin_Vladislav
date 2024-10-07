ed9960612e23400978512a3137a9de90f6241ea1
vladjkezor <savinv31@gmail.com>
2024-10-07 23:28:13 +0300

    fixed tests(added checking boundary conditions and predict calls)

--- 01/test_prediction_model.py
+++ 01/test_prediction_model.py
@@ -1,4 +1,5 @@
 import unittest
+from unittest import mock
 from unittest.mock import patch
 
 from prediction_model import predict_message_mood
@@ -9,9 +10,17 @@ class TestPredictMessageMood(unittest.TestCase):
     @patch('prediction_model.SomeModel.predict')
     def test_mood_no_borders(self, mock_predict):
         mock_predict.side_effect = [0.6, 0.9, 0.2]
+
         self.assertEqual(predict_message_mood('Чапаев и Пустота'), 'норм')
+        mock_predict.assert_called_once_with('Чапаев и Пустота')
+        mock_predict.reset_mock()
+
         self.assertEqual(predict_message_mood('Круть'), 'отл')
+        mock_predict.assert_called_once_with('Круть')
+        mock_predict.reset_mock()
+
         self.assertEqual(predict_message_mood('Война и Мир'), 'неуд')
+        mock_predict.assert_called_once_with('Война и Мир')
 
     @patch('prediction_model.SomeModel.predict')
     def test_mood_with_borders(self, mock_predict):
@@ -19,6 +28,12 @@ class TestPredictMessageMood(unittest.TestCase):
         self.assertEqual(predict_message_mood('Какой-то', 0.8, 0.9), 'отл')
         self.assertEqual(predict_message_mood('Текст', 0.4, 0.6), 'норм')
         self.assertEqual(predict_message_mood('Вот', 0.7, 0.95), 'неуд')
+
+        calls = [mock.call('Какой-то'),
+                 mock.call('Текст'),
+                 mock.call('Вот')]
+        self.assertEqual(mock_predict.call_args_list, calls)
+        self.assertEqual(mock_predict.call_count, 3)
 
     def test_invalid_borders(self):
         with self.assertRaises(ValueError) as err:
@@ -47,6 +62,21 @@ class TestPredictMessageMood(unittest.TestCase):
         with self.assertRaises(ValueError) as err:
             predict_message_mood('Модель ничего не вернула')
         self.assertEqual(str(err.exception), 'No prediction')
+
+    @patch('prediction_model.SomeModel.predict')
+    def test_boundary_conditions(self, mock_predict):
+        mock_predict.side_effect = [0.3 - 10 ** (-10),
+                                    0.3,
+                                    0.3 + 10 ** (-10),
+                                    0.8,
+                                    0.8 - 10 ** (-10),
+                                    0.8 + 10 ** (-10)]
+        self.assertEqual(predict_message_mood('left_bound'), 'неуд')
+        self.assertEqual(predict_message_mood('left_left_bound'), 'норм')
+        self.assertEqual(predict_message_mood('right_left_bound'), 'норм')
+        self.assertEqual(predict_message_mood('right_bound'), 'норм')
+        self.assertEqual(predict_message_mood('left_right_bound'), 'норм')
+        self.assertEqual(predict_message_mood('right_right_bound'), 'отл')
 
 
 if __name__ == '__main__':
