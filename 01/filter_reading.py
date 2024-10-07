b338c358759d509d25a01ccf3eaf91b39b86fbe8
vladjkezor <savinv31@gmail.com>
2024-10-08 01:41:18 +0300

    added reading both file and filename, addet more tests

--- 01/filter_reading.py
+++ 01/filter_reading.py
@@ -1,13 +1,22 @@
 import os
 
 
-def reader_with_filter(filename: str, key_words: list, stop_words: list):
+def reader_with_filter(file_or_filename, key_words: list, stop_words: list):
     key_words = {word.lower() for word in key_words}
     stop_words = {word.lower() for word in stop_words}
-    if not os.path.isfile(filename):
-        raise FileNotFoundError(f"The file '{filename}' does not exist.")
-    with open(filename, 'r', encoding='utf-8') as text:
-        for line in text:
-            words_to_check = set(line.lower().split())
-            if key_words & words_to_check and not stop_words & words_to_check:
+    if isinstance(file_or_filename, str):
+        if not os.path.isfile(file_or_filename):
+            raise FileNotFoundError(f"The file '{file_or_filename}'"
+                                    f" does not exist.")
+        # Если передано название файла
+        with open(file_or_filename, 'r', encoding='utf-8') as file:
+            for line in file:
+                check_words = set(line.lower().split())
+                if key_words & check_words and not stop_words & check_words:
+                    yield line.strip()
+    # Если передан файл
+    else:
+        for line in file_or_filename:
+            check_words = set(line.lower().split())
+            if key_words & check_words and not stop_words & check_words:
                 yield line.strip()
--- 01/test_filter_reading.py
+++ 01/test_filter_reading.py
@@ -12,27 +12,123 @@ class TestReaderWithFilter(unittest.TestCase):
         with self.assertRaises(FileNotFoundError):
             list(reader_with_filter(wrong_filename, key_words, stop_words))
 
-    def test_file_filter(self):
+    def test_case_insensitive(self):
         file_name = 'test_file.txt'
-        key_words = ['key', 'banana']
-        stop_words = ['stop']
+        key_words = ['apple', 'banana']
+        stop_words = ['STOP', 'end']
 
-        with open(file_name, 'w', encoding='utf-8') as file:
-            file.write('Line with key word\n')
-            file.write('Line with two key key words\n')
-            file.write('Line with no search words\n')
-            file.write('Line with two different key banana words\n')
-            file.write('Line with stop and Key word\n')
-            file.write('Line with capitalized KEY word\n')
-            file.write('Line with stop word\n')
+        with open(file_name, 'w', encoding='utf-8') as f:
+            f.write('APPLE is a fruit\n')
+            f.write('BAnaNA is yellow\n')
+            f.write('stop\n')
+            f.write('here we have End\n')
+            f.write('banana End\n')
 
-        expected = [
-            'Line with key word',
-            'Line with two key key words',
-            'Line with two different key banana words',
-            'Line with capitalized KEY word'
-        ]
+        expected = ['APPLE is a fruit',
+                    'BAnaNA is yellow']
+
         result = list(reader_with_filter(file_name, key_words, stop_words))
+        self.assertEqual(expected, result)
+
+    def test_with_multiple_key_words_in_line(self):
+        file_name = 'test_file.txt'
+        key_words = ['apple', 'banana']
+        stop_words = ['STOP', 'end']
+
+        with open(file_name, 'w', encoding='utf-8') as f:
+            f.write('apple and banana are sweet')
+
+        expected = ['apple and banana are sweet']
+
+        result = list(reader_with_filter(file_name, key_words, stop_words))
+        self.assertEqual(expected, result)
+
+    def test_with_empty_file(self):
+        file_name = 'test_file.txt'
+        key_words = ['apple', 'banana']
+        stop_words = ['STOP', 'end']
+
+        with open(file_name, 'w', encoding='utf-8') as f:
+            f.write('')
+
+        expected = []
+
+        result = list(reader_with_filter(file_name, key_words, stop_words))
+        self.assertEqual(expected, result)
+
+    def test_with_empty_keywords(self):
+        file_name = 'test_file.txt'
+        key_words = []
+        stop_words = ['STOP', 'end']
+
+        with open(file_name, 'w', encoding='utf-8') as f:
+            f.write('APPLE is a fruit\n')
+            f.write('BAnaNA is yellow\n')
+            f.write('stop\n')
+
+        expected = []
+
+        result = list(reader_with_filter(file_name, key_words, stop_words))
+        self.assertEqual(expected, result)
+
+    def test_equal_key_and_stop_words(self):
+        file_name = 'test_file.txt'
+        key_words = ['apple', 'banana']
+        stop_words = ['apple']
+
+        with open(file_name, 'w', encoding='utf-8') as f:
+            f.write('APPLE is a fruit\n')
+            f.write('BAnaNA is yellow\n')
+
+        expected = ['BAnaNA is yellow']
+
+        result = list(reader_with_filter(file_name, key_words, stop_words))
+        self.assertEqual(expected, result)
+
+    def test_partial_word_match(self):
+        file_name = 'test_file.txt'
+        key_words = ['key']
+        stop_words = []
+
+        with open(file_name, 'w', encoding='utf-8') as f:
+            f.write('keyboard\n')
+            f.write('key\n')
+
+        expected = ['key']
+
+        result = list(reader_with_filter(file_name, key_words, stop_words))
+        self.assertEqual(expected, result)
+
+    def test_full_line_match(self):
+        file_name = 'test_file.txt'
+        key_words = ['banana is red']
+        stop_words = ['stop key is key']
+
+        with open(file_name, 'w', encoding='utf-8') as f:
+            f.write('banana is red\n')
+            f.write('stop key is key\n')
+
+        expected = []
+
+        result = list(reader_with_filter(file_name, key_words, stop_words))
+        self.assertEqual(expected, result)
+
+    def test_file_obj(self):
+        file_name = 'test_file.txt'
+        key_words = ['is']
+        stop_words = ['STOP', 'end']
+
+        with open(file_name, 'w', encoding='utf-8') as f:
+            f.write('APPLE is a fruit\n')
+            f.write('BAnaNA is yellow\n')
+            f.write('Is stop\n')
+
+        expected = ['APPLE is a fruit',
+                    'BAnaNA is yellow']
+
+        with open(file_name, 'r', encoding='utf-8') as file_obj:
+            result = list(reader_with_filter(file_obj, key_words, stop_words))
+
         self.assertEqual(expected, result)
 
 
