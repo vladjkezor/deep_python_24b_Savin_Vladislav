b25f78d59dcc6545425c88dca03ee2130bc8fc3d
vladjkezor <savinv31@gmail.com>
2024-10-10 20:52:56 +0300

    added comparison, str and invalid type tests

--- 03/custom_list.py
+++ 03/custom_list.py
@@ -8,14 +8,14 @@ class CustomList(list):
             return result
         if isinstance(other, (int, float)):
             return CustomList([x + other for x in self])
-        return NotImplemented
+        raise TypeError()
 
     def __radd__(self, other):
         if isinstance(other, (CustomList, list)):
             return self.__add__(other)
         if isinstance(other, (int, float)):
             return CustomList([x + other for x in self])
-        return NotImplemented
+        raise TypeError()
 
     def __sub__(self, other):
         if isinstance(other, (CustomList, list)):
@@ -26,7 +26,7 @@ class CustomList(list):
             return result
         if isinstance(other, (int, float)):
             return CustomList([x - other for x in self])
-        return NotImplemented
+        raise TypeError()
 
     def __rsub__(self, other):
         if isinstance(other, (CustomList, list)):
@@ -37,37 +37,37 @@ class CustomList(list):
             return result
         if isinstance(other, (int, float)):
             return CustomList([other - x for x in self])
-        return NotImplemented
+        raise TypeError()
 
     def __lt__(self, other):
         if isinstance(other, CustomList):
             return sum(self) < sum(other)
-        return NotImplemented
+        raise TypeError()
 
     def __le__(self, other):
         if isinstance(other, CustomList):
             return sum(self) <= sum(other)
-        return NotImplemented
+        raise TypeError()
 
     def __eq__(self, other):
         if isinstance(other, CustomList):
             return sum(self) == sum(other)
-        return NotImplemented
+        raise TypeError()
 
     def __ne__(self, other):
         if isinstance(other, CustomList):
             return sum(self) != sum(other)
-        return NotImplemented
+        raise TypeError()
 
     def __gt__(self, other):
         if isinstance(other, CustomList):
             return sum(self) > sum(other)
-        return NotImplemented
+        raise TypeError()
 
     def __ge__(self, other):
         if isinstance(other, CustomList):
             return sum(self) >= sum(other)
-        return NotImplemented
+        raise TypeError()
 
     def __str__(self):
-        return f' {super().__str__()} sum = {str(sum(self))}'
+        return f'{super().__str__()} sum = {str(sum(self))}'
