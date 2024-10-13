4f341c0fe6ef044c4ff57ab3efc989f690f2a4ce
vladjkezor <savinv31@gmail.com>
2024-10-13 12:12:27 +0300

    Add some comments to metaclass code

--- 04/metaclass.py
+++ 04/metaclass.py
@@ -6,6 +6,7 @@ class CustomMeta(type):
             attr_name = mcs.is_magic(attr_name)
             new_dict[attr_name] = attr_value
 
+        # Замена сетатра для экземпляров класса
         def custom_setattr(self, key, value):
             key = mcs.is_magic(key)
             object.__setattr__(self, key, value)
@@ -14,11 +15,13 @@ class CustomMeta(type):
 
         return super().__new__(mcs, name, bases, new_dict)
 
+    # Замена сеттера для класса
     def __setattr__(cls, key, value):
         key = cls.is_magic(key)
         return super().__setattr__(key, value)
 
     @staticmethod
+    # Добавляет префикс к строке если это не название магического метода
     def is_magic(key):
         if not (key.startswith('__') and key.endswith('__')):
             return f'custom_{key}'
