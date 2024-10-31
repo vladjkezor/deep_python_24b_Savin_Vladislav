630f424757fe884d42452e797abcf8683ea0208d
vladjkezor <savinv31@gmail.com>
2024-11-01 01:00:15 +0300

    Add threading to client

--- 06/client.py
+++ 06/client.py
@@ -1,10 +1,41 @@
+import threading
+import queue
 import socket
 
-URL = "http://ru.wikipedia.org/wiki/Python"
-URLS = [URL] * 20
+URL = "http://ru.wikipeia.org/"
+URLS = [URL] * 10
 
-for i in range(20):
-    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as client:
-        client.connect(('localhost', 20_000))
-        client.sendall(URL.encode())
 
+class ClientWorker(threading.Thread):
+    def __init__(self, que):
+        super().__init__()
+        self.que = que
+
+    def run(self):
+        while True:
+            url = self.que.get()
+            if url is None:
+                self.que.put(None)
+                break
+
+            with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as client:
+                client.connect(('localhost', 20_000))
+                client.sendall(url.encode())
+
+                data = client.recv(1024).decode()
+                print(f' {url} {data}')
+
+
+class Client:
+    def __init__(self, urls, n_workers):
+        self.que = queue.Queue()
+        for url in urls:
+            self.que.put(url)
+        self.que.put(None)
+
+        self.workers = [ClientWorker(self.que) for _ in range(n_workers)]
+        for worker in self.workers:
+            worker.start()
+
+
+cl = Client(URLS, 40)
--- 06/server.py
+++ 06/server.py
@@ -8,18 +8,38 @@ from collections import Counter
 
 class Worker(threading.Thread):
 
-    def __init__(self, que, top_k):
+    def __init__(self, que, top_k, server):
         super().__init__()
         self.que = que
         self.top_k = top_k
+        self.server = server
 
     def run(self):
         while True:
             connection = self.que.get()
-            url = connection.recv(1024).decode()
+            if connection is None:
+                self.que.put(None)
+                break
+            try:
+                url = connection.recv(1024).decode()
+                connection.sendall(self.fetch_and_process_url(url))
+            except Exception as e:
+                print(f"error {e}")
+            finally:
+                connection.close()
+                with self.server.lock:
+                    self.server.n_processed += 1
+                    print(f'processed {self.server.n_processed} urls')
+
+    def fetch_and_process_url(self, url):
+        try:
             data = requests.get(url).text.split()
-            print(f'{self.name} --- {Counter(data).most_common(self.top_k)}')
-            connection.close()
+            top_words = dict(Counter(data).most_common(self.top_k))
+            return json.dumps(top_words).encode()
+
+        except requests.RequestException as e:
+            print(f"Failed to fetch {url}: {e}")
+            return f'{e}'.encode()
 
 
 class Server:
@@ -27,7 +47,9 @@ class Server:
     def __init__(self, n_workers, top_k):
         self.top_k = top_k
         self.que = queue.Queue()
-        self.workers = [Worker(self.que, top_k) for _ in range(n_workers)]
+        self.n_processed = 0
+        self.lock = threading.Lock()
+        self.workers = [Worker(self.que, top_k, self) for _ in range(n_workers)]
 
         for worker in self.workers:
             worker.start()
@@ -38,11 +60,14 @@ class Server:
             sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
             sock.bind(("localhost", 20_000))
             sock.listen()
+            try:
+                while True:
+                    client, addr = sock.accept()
+                    self.que.put(client)
+            except KeyboardInterrupt:
+                print('Server closed')
+                self.que.put(None)
 
-            while True:
-                client, addr = sock.accept()
-                self.que.put(client)
 
-
-serv = Server(5, 3)
+serv = Server(3, 3)
 serv.start()
