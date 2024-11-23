423e39d3e61a4b74ab76198ef57a72845f767ae3
vladjkezor <savinv31@gmail.com>
2024-11-24 02:51:15 +0300

    Add performance testing for different types of attrs

--- 08/performance_analysis.py
+++ 08/performance_analysis.py
@@ -1,63 +1,56 @@
-# import weakref
+import weakref
 from time import time
-import random as rand
-
-names = [
-    "James", "Mary", "Robert", "Patricia", "John", "Jennifer", "Michael",
-    "Linda", "William", "Elizabeth", "David", "Barbara", "Richard", "Susan",
-    "Joseph", "Jessica", "Thomas", "Sarah", "Charles", "Karen", "Christopher",
-    "Nancy", "Daniel", "Margaret", "Matthew", "Lisa", "Anthony", "Betty",
-    "Donald", "Dorothy", "Mark", "Sandra", "Paul", "Ashley", "Steven",
-    "Kimberly", "Andrew", "Donna", "Kenneth", "Emily", "Joshua", "Michelle",
-    "George", "Carol", "Kevin", "Amanda", "Brian", "Melissa", "Edward",
-    "Deborah"]
-
-surnames = [
-    "Smith", "Johnson", "Williams", "Brown", "Jones", "Garcia", "Miller",
-    "Davis", "Rodriguez", "Martinez", "Hernandez", "Lopez", "Gonzalez",
-    "Wilson", "Anderson", "Thomas", "Taylor", "Moore", "Jackson", "Martin",
-    "Lee", "Perez", "Thompson", "White", "Harris", "Sanchez", "Clark",
-    "Ramirez", "Lewis", "Robinson", "Walker", "Young", "Allen", "King",
-    "Wright", "Scott", "Torres", "Nguyen", "Hill", "Flores", "Green",
-    "Adams", "Nelson", "Baker", "Hall", "Rivera", "Campbell", "Mitchell",
-    "Carter", "Roberts"]
 
 
 class Book:
-
-    def __init__(self, title, author, publication_year):
+    def __init__(self, title, publication_year):
         self.title = title
-        self.author = author
         self.year = publication_year
-
-    def __str__(self):
-        return f'{self.title}, {self.author}, {self.year}'
 
 
 class SlotsBook:
-    __slots__ = ('title', 'author', 'year')
+    __slots__ = ('title', 'year')
 
-    def __init__(self, title, author, publication_year):
+    def __init__(self, title, publication_year):
         self.title = title
-        self.author = author
         self.year = publication_year
 
 
+class WeakRefBook:
+    def __init__(self, title, publication_year):
+        self.title = weakref.ref(title)
+        self.year = weakref.ref(publication_year)
+
+
+class RefClass:
+    def __init__(self, value):
+        self.value = value
+
+
 def measure_performance_time(cls, n_instances):
-    t_start = time()
-    books = [cls(f'Book_{i}',
-                 i,
-                 rand.randint(1800, 2025))
-             for i in range(n_instances)]
+    init_time = 0
+    changing_attr_time = 0
+    for _ in range(3):
+        t_start = time()
+        books = [cls(RefClass('title'),
+                     RefClass(2010))
+                 for _ in range(n_instances)]
+        init_time += time() - t_start
 
-    for book in books:
-        book.title = book.title.split('_')[1] + '_book'
-        book.year = rand.randint(1500, 1700)
+        t_start = time()
+        for book in books:
+            _ = book.title
+            _ = book.year
+            book.title = RefClass('new_title')
+            book.year = RefClass(2024)
+        changing_attr_time += time() - t_start
 
-    t_end = time()
+    print(f'{cls.__name__} avg init time = {init_time / 3}')
+    print(f'{cls.__name__} avg changing attr time = {changing_attr_time / 3}')
 
-    print(f'{cls.__name__} init time = {t_end - t_start}')
 
-
-measure_performance_time(Book, 10_000_000)
-measure_performance_time(SlotsBook, 10_000_000)
+N = 20_000_000
+print(f'Number of instances = {N}')
+measure_performance_time(Book, N)
+measure_performance_time(SlotsBook, N)
+measure_performance_time(WeakRefBook, N)
