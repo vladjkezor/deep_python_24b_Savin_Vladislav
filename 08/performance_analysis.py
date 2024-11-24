616442f5002c27d481e39428ed8c149987e046da
vladjkezor <savinv31@gmail.com>
2024-11-25 02:17:06 +0300

    Add hw8

--- 08/performance_analysis.py
+++ 08/performance_analysis.py
@@ -3,6 +3,7 @@ from time import time
 import cProfile
 import pstats
 import io
+
 from memory_profiler import profile
 
 
@@ -32,37 +33,42 @@ class RefClass:
 
 
 @profile
-def measure_performance_time(cls, n_instances):
+def measure_performance_time(cls, n_instances, n_retries):
     init_time = 0
     changing_attr_time = 0
-    for _ in range(3):
+    for _ in range(n_retries):
         t_start = time()
-        books = [cls(RefClass('title'),
+        books = [cls(RefClass(f'title_{i}'),
                      RefClass(2010))
-                 for _ in range(n_instances)]
+                 for i in range(n_instances)]
         init_time += time() - t_start
 
         t_start = time()
         for book in books:
             _ = book.title() if isinstance(book.title, weakref.ref) \
                 else book.title
-            _ = book.year() if isinstance(book.year, weakref.ref) else book.year
+            _ = book.year() if isinstance(book.year, weakref.ref) \
+                else book.year
             book.title = RefClass('new_title')
             book.year = RefClass(2024)
         changing_attr_time += time() - t_start
 
-    print(f'{cls.__name__} avg init time = {init_time / 3}')
-    print(f'{cls.__name__} avg changing attr time = {changing_attr_time / 3}')
+    print(f'{cls.__name__} avg init time = {init_time / n_retries}')
+    print(f'{cls.__name__} '
+          f'avg changing attr time = {changing_attr_time / n_retries}')
 
 
 if __name__ == '__main__':
     pr = cProfile.Profile()
     pr.enable()
-    N = 20_000_000
-    print(f'Number of instances = {N}')
-    measure_performance_time(Book, N)
-    measure_performance_time(SlotsBook, N)
-    measure_performance_time(WeakRefBook, N)
+
+    N = 10_000_000
+    N_RETRIES = 3
+    print(f'Number of instances = {N}, Number of retries = {N_RETRIES}')
+    measure_performance_time(Book, N, N_RETRIES)
+    measure_performance_time(SlotsBook, N, N_RETRIES)
+    measure_performance_time(WeakRefBook, N, N_RETRIES)
+
     pr.disable()
     s = io.StringIO()
     SORT_BY = 'cumulative'
---
+++ 08/screenshots.md
@@ -0,0 +1,6 @@
+### 1. Сравнение использования обычных атрибутов, weakref и слотов
+![](https://github.com/vladjkezor/deep_python_24b_Savin_Vladislav/blob/feature/08/class_time_comparsion.jpg)
+### 2. Профилирование по вызовам
+![](https://github.com/vladjkezor/deep_python_24b_Savin_Vladislav/blob/feature/08/calls_profile.jpg)
+### 3. Профилирование по памяти
+![](https://github.com/vladjkezor/deep_python_24b_Savin_Vladislav/blob/feature/08/memory_profile.jpg)
