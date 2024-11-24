529fda7ef438021ef440756d5ddb063793c6f510
vladjkezor <savinv31@gmail.com>
2024-11-24 17:56:29 +0300

    Add call and time profile

--- 08/performance_analysis.py
+++ 08/performance_analysis.py
@@ -1,5 +1,9 @@
 import weakref
 from time import time
+import cProfile
+import pstats
+import io
+from memory_profiler import profile
 
 
 class Book:
@@ -27,6 +31,7 @@ class RefClass:
         self.value = value
 
 
+@profile
 def measure_performance_time(cls, n_instances):
     init_time = 0
     changing_attr_time = 0
@@ -39,8 +44,9 @@ def measure_performance_time(cls, n_instances):
 
         t_start = time()
         for book in books:
-            _ = book.title
-            _ = book.year
+            _ = book.title() if isinstance(book.title, weakref.ref) \
+                else book.title
+            _ = book.year() if isinstance(book.year, weakref.ref) else book.year
             book.title = RefClass('new_title')
             book.year = RefClass(2024)
         changing_attr_time += time() - t_start
@@ -49,8 +55,17 @@ def measure_performance_time(cls, n_instances):
     print(f'{cls.__name__} avg changing attr time = {changing_attr_time / 3}')
 
 
-N = 20_000_000
-print(f'Number of instances = {N}')
-measure_performance_time(Book, N)
-measure_performance_time(SlotsBook, N)
-measure_performance_time(WeakRefBook, N)
+if __name__ == '__main__':
+    pr = cProfile.Profile()
+    pr.enable()
+    N = 20_000_000
+    print(f'Number of instances = {N}')
+    measure_performance_time(Book, N)
+    measure_performance_time(SlotsBook, N)
+    measure_performance_time(WeakRefBook, N)
+    pr.disable()
+    s = io.StringIO()
+    SORT_BY = 'cumulative'
+    ps = pstats.Stats(pr, stream=s).sort_stats(SORT_BY)
+    ps.print_stats()
+    print(s.getvalue())
