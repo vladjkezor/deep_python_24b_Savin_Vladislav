083da3985de0d00679daa03a1bef26d3f62aff74
vladjkezor <savinv31@gmail.com>
2024-11-30 19:30:12 +0300

    Add logs

--- 09/lru_cache.py
+++ 09/lru_cache.py
@@ -4,32 +4,46 @@ from lru_logger import create_logger, add_stream
 
 lru_logger = create_logger()
 
+
 class LRUCache:
 
     def __init__(self, limit=42):
         if not isinstance(limit, int):
+            lru_logger.error('Limit not integer')
             raise TypeError('Limit should be integer')
         if limit <= 0:
+            lru_logger.error('Limit not positive')
             raise ValueError('Limit should be positive')
         self.limit = limit
         self.cache = {}
+        lru_logger.info('LRUCache initialized with limit %s', self.limit)
 
     def get(self, key):
         if key in self.cache:
             temp = self.cache[key]
             del self.cache[key]
             self.cache[key] = temp
+            lru_logger.info('Key %s accessed successfully', key)
+            lru_logger.debug('cache = %s', self.cache)
             return temp
-
+        lru_logger.warning('Key %s is not in the cache', key)
         return None
 
     def set(self, key, value):
         if key in self.cache:
             del self.cache[key]
-        if len(self.cache) >= self.limit:
-            old_key = next(iter(self.cache))
-            del self.cache[old_key]
-        self.cache[key] = value
+            self.cache[key] = value
+            lru_logger.info('Key %s updated with new value %s', key, value)
+        else:
+            if len(self.cache) >= self.limit:
+                old_key = next(iter(self.cache))
+                del self.cache[old_key]
+                lru_logger.info('Cache limit reached. '
+                                'Oldest key %s removed', old_key)
+            self.cache[key] = value
+            lru_logger.info('Key %s added to cache with value %s', key, value)
+
+        lru_logger.debug('cache = %s', self.cache)
 
 
 if __name__ == '__main__':
@@ -39,8 +53,9 @@ if __name__ == '__main__':
     args = parser.parse_args()
 
     if args.stdout:
-        print('pop')
+        add_stream(lru_logger)
     if args.filter:
-        print('kek')
+        pass
 
-    cache = LRUCache(3)
+
+
